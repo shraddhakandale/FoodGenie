4f97ba1f35e100c37afe67a162a1c56a15a65470
shraddhakandale <sorkeyy@gmail.com>
2025-11-30 20:59:46 +0530

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-2 h-12 bg-orange-600 w-full flex justify-center items-center">
+      <span className="text-white sm:font-bold text-sm">
+        © FoodieGenie. Developed by Shraddha Kandale. All rights reserved.
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,16 @@
+import Footer from "./Footer";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="min-h-screen bg-orange-50">
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <div className="min-h-screen bg-orange-50">
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 }
 
